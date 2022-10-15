6cf04cbad221e79d9756c5870ae7b317e53af8f7
gardenia1109 <gardenia1109@naver.com>
2022-10-15 22:17:41 +0900

    Initial Commit

---
+++ Cal/CalApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalApp.swift
+//  Cal
+//
+//  Created by 정선영 on 2022/10/15.
+//
+
+import SwiftUI
+
+@main
+struct CalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
